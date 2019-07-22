8439764274e107ca18450273759d58f46280bac6
Jesus Zuniga <jesus.zuniga@macys.com>
2019-07-22 17:24:41 -0400

    Different eager and lazy cases

--- src/com/luv2code/hibernate/demo/FetchJoinDemo.java
+++ src/com/luv2code/hibernate/demo/FetchJoinDemo.java
@@ -6,9 +6,10 @@ import com.luv2code.hibernate.demo.entity.InstructorDetail;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 
-public class EagerLazyDemo {
+public class FetchJoinDemo {
 
     public static void main(String[] args) {
         // create session factory
@@ -27,17 +28,32 @@ public class EagerLazyDemo {
             // start a transaction
             session.beginTransaction();
 
+            // option 2: Hibernate query with HQL
+
             // get the instructor from db
             int theId = 1;
-            Instructor tempInstructor = session.get(Instructor.class, theId);
+
+            Query<Instructor> query = session.createQuery("select i from Instructor i "
+                    + "JOIN FETCH i.courses "
+                    + "where i.id=:theInstructorId", Instructor.class);
+
+            // set parameter on query
+            query.setParameter("theInstructorId", theId);
+
+            // execute query and get instructor
+            Instructor tempInstructor = query.getSingleResult();
 
             System.out.println("[luv2code]: Instructor: " + tempInstructor);
 
-            // get courses for the instructor
-            System.out.println("[luv2code]: Courses: " + tempInstructor.getCourses());
-
             // commit transaction
             session.getTransaction().commit();
+
+            // close the session
+            session.close();
+            System.out.println("\nluv2code: The session is now closed!\n");
+
+            // get courses for the instructor
+            System.out.println("luv2code: Courses: " + tempInstructor.getCourses());
 
             System.out.println("Done!");
         }
